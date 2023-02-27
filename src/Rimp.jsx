a5a87a4d5b0582c4adb8d94bf874511e8798d779
Thaddaeus <thaddaeusotieno@gmail.com>
2023-02-27 16:18:27 +0300

    Todo

---
+++ src/Rimp.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+    
+  function Rimp() {
+    const [trail = setTrail] = useState({
+        id: "",
+        name: "",
+        trail: ""
+    })
+      return (
+        <div>
+            <input type="checkbox" />
+            <p>List Item O1 {()=>setTrail = {
+             main: ""   
+           }}</p>
+           <button>delete</button>
+        </div>
+      )
+    }
+    
+export default Rimp;
